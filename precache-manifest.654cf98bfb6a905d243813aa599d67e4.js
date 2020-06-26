0e6d74ef4d47d19c0fe44012d013b43acc462bfd
astroProject <akshayhemant@hotmail.com>
2020-06-26 12:49:07 +0530

    Updates

---
+++ precache-manifest.654cf98bfb6a905d243813aa599d67e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ad1292fde6731096ba83596b7069f3",
+    "url": "/Covid19-tracker/index.html"
+  },
+  {
+    "revision": "079c29db83ee49890b45",
+    "url": "/Covid19-tracker/static/css/main.f238096f.chunk.css"
+  },
+  {
+    "revision": "da57a3b5155dd4b6ee2f",
+    "url": "/Covid19-tracker/static/js/2.52259247.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19-tracker/static/js/2.52259247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079c29db83ee49890b45",
+    "url": "/Covid19-tracker/static/js/main.56bd2aa5.chunk.js"
+  },
+  {
+    "revision": "bbd98028698a9ad253a0",
+    "url": "/Covid19-tracker/static/js/runtime-main.060112c8.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19-tracker/static/media/covid19.d7265326.png"
+  }
+]);
